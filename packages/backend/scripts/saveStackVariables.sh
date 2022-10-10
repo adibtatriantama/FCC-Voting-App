7228252fc11180fe15157953dbba4e65c5e6dd94
Adibta Triantama <adibtatriantama@gmail.com>
2022-10-10 18:23:40 +0700

    ci(backend): fix saveStackVariables script

--- packages/backend/scripts/saveStackVariables.sh
+++ packages/backend/scripts/saveStackVariables.sh
@@ -1,6 +1,6 @@
 APP_REGION=`sls output get --name appRegion --stage test`
 TABLE_NAME=`sls output get --name tableName --stage test`
 LAMBDA_FUNCTION_NAME_PREFIX=`sls output get --name lambdaFunctionNamePrefix --stage test`
-echo ::set-output name=APP_REGION::$APP_REGION
-echo ::set-output name=TABLE_NAME::$TABLE_NAME
-echo ::set-output name=LAMBDA_FUNCTION_NAME_PREFIX::$LAMBDA_FUNCTION_NAME_PREFIX
+echo "::set-output name=APP_REGION::$APP_REGION"
+echo "::set-output name=TABLE_NAME::$TABLE_NAME"
+echo "::set-output name=LAMBDA_FUNCTION_NAME_PREFIX::$LAMBDA_FUNCTION_NAME_PREFIX"
